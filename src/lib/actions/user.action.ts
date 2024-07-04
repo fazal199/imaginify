0f56b322b6676ffb0a071220df8e3253515c5280
fazal199 <fazalhussain1441@gmail.com>
2024-07-04 16:16:04 +0530

    fix middleware of clerk

--- src/lib/actions/user.action.ts
+++ src/lib/actions/user.action.ts
@@ -1,8 +1,6 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-
-
 import { User } from "../database/models/user.model";
 import { connectDb } from "../database/dbConfig";
 import { handleError } from "../utils";
@@ -46,7 +44,7 @@ export async function updateUser(clerkId: string, user: UpdateUserParams) {
     });
 
     if (!updatedUser) throw new Error("User update failed");
-    
+
     return JSON.parse(JSON.stringify(updatedUser));
   } catch (error) {
     handleError(error);
@@ -82,16 +80,14 @@ export async function updateCredits(userId: string, creditFee: number) {
 
     const updatedUserCredits = await User.findOneAndUpdate(
       { _id: userId },
-      { $inc: { creditBalance: creditFee }},
+      { $inc: { creditBalance: creditFee } },
       { new: true }
-    )
+    );
 
-    if(!updatedUserCredits) throw new Error("User credits update failed");
+    if (!updatedUserCredits) throw new Error("User credits update failed");
 
     return JSON.parse(JSON.stringify(updatedUserCredits));
   } catch (error) {
     handleError(error);
   }
 }
-
-
