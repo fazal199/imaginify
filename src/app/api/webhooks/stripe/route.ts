11bd42529f20778189ab5621039d71a72d09e4fb
fazal199 <fazalhussain1441@gmail.com>
2024-07-04 17:19:15 +0530

    try to fix the stripe webhook error

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -1,11 +1,12 @@
 /* eslint-disable camelcase */
 import { createTransaction } from "./../../../../lib/actions/transaction.action";
 import { NextResponse } from "next/server";
-import stripe from "stripe";
+import {Stripe} from "stripe";
 
 export async function POST(request: Request) {
   const body = await request.text();
 
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
   const sig = request.headers.get("stripe-signature") as string;
   const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
 
